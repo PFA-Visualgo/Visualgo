99591fad8e32e5e987107506272b62a5f3dc5a78
ttrenty <154608953+ttrenty@users.noreply.github.com>
2024-01-07 08:41:56 +0100

    add js and python functions for buttons + better code input

--- src/main.py
+++ src/main.py
@@ -1,9 +1,55 @@
 import arrr
 from pyscript import document
 
-
 def translate_english(event):
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
+
+def selectFunctions(event):
+    # Implement the logic for the Select Functions button
+    pass
+
+def selectVariables(event):
+    # Implement the logic for the Select Variables button
+    pass
+
+def start(event):
+    button = document.querySelector("#restartButton")
+    if button.innerText == "Start":
+        button.innerText = "Restart"
+    elif button.innerText == "Restart":
+        button.innerText = "Start"
+    pauseButton = document.querySelector("#pauseButton")
+    pauseButton.innerText = "Pause"
+
+    output_div = document.querySelector("#drawingArea")
+    output_div.innerText = "Start"
+    pass
+
+def pause(event):
+    button = document.querySelector("#pauseButton")
+    if button.innerText == "Pause":
+        button.innerText = "Resume"
+    elif button.innerText == "Resume":
+        button.innerText = "Pause"
+    pass
+
+def stepBackward(event):
+    # Implement the logic for the Step Backward button
+    pass
+
+def nextStepBackward(event):
+    # Implement the logic for the Next Step Backward button
+    pass
+
+def stepForward(event):
+    # Implement the logic for the Step Forward button
+    pass
+
+def nextStepForward(event):
+    # Implement the logic for the Next Step Forward button
+    pass
+
+
