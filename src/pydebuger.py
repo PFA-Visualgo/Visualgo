4a260e68b6f2a14a4be425346f58a3124fa7efdc
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-01-25 14:08:48 +0100

    semi-PoC of a debugger

--- src/pydebuger.py
+++ src/pydebuger.py
@@ -1,6 +1,12 @@
-import pdb
+import bdb
+from types import FrameType
 
-def runLineOfCode(line: str):
-    # pdb.runeval(expression, globals=None, locals=None)
-    res =  pdb.runeval(line)
-    return res
+class MyBdb(bdb.Bdb):
+    def user_line(self, frame: FrameType) -> None:
+        print("USER LINE AT FRAME")
+        print(frame)
+
+# def runLineOfCode(line: str):
+#     # pdb.runeval(expression, globals=None, locals=None)
+#     res =  pdb.runeval(line)
+#     return res
