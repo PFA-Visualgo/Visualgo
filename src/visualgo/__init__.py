92653c0a426265d8c7d9f6e1b79400e1e17b6ea9
ttrenty <154608953+ttrenty@users.noreply.github.com>
2024-02-04 12:38:53 +0100

    Reorganise repo and add files needed for a basic Pypi project

---
+++ src/visualgo/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# Version of the visualgo package
+__version__ = "2024.2.4"
---
+++ src/visualgo/__main__.py
@@ -0,0 +1,9 @@
+# __main__.py
+
+def main():
+    """It's in development."""
+
+    print("This is the visualgo package. It's in development...")
+
+if __name__ == "__main__":
+    main()
