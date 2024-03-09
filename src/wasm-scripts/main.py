c146a1faa3af9d80548fcbad4de34e4958ebb468
Arthur Le Floch <115921016+ArthurLeFloch@users.noreply.github.com>
2024-03-09 10:59:24 +0100

    Add worker thread, move pyscript files to src/wasm-scripts (#31)
    * Callback tests, with build and pyscript.json
    
    * Include worker thread in src/ui, callbacks from ui to dbg and the other way round
    
    * Same as /junk/logic/ui-to-controller, without actual calls from ui to controller
    
    * Fix pyscript 'a' error in console
    
    * Remove debugger classes from src/ui
    
    * feat: select either python or python3
    
    ---------
    
    Co-authored-by: ttrenty <154608953+ttrenty@users.noreply.github.com>

--- src/wasm-scripts/main.py
+++ src/wasm-scripts/main.py
@@ -5,23 +5,49 @@ from visualgo.logic import Controller, PyDebugger
 
 from pyscript_ui_callbacks import PyscriptUICallbacks
 
+
 class Visualisation:
-    """
-    Class that acts as the main interface between the frontend and the backend.
-    """
 
     def __init__(self):
-        self.__controller = Controller(PyDebugger, PyscriptUICallbacks())
+        self.controller = Controller(PyDebugger, PyscriptUICallbacks())
+        self.code = "print('Hello, World!')"
 
     def start(self, _):
-        self.__controller.start()
-        pass
+        self.controller.start()
+
+    def forward_step(self, _):
+        self.controller.forward_step()
+
+    def forward_next(self, _):
+        self.controller.forward_next()
+
+    def backward_step(self, _):
+        self.controller.backward_step()
+
+    def backward_next(self, _):
+        self.controller.backward_next()
 
 
 if __name__ == "__main__":
     visualisation = Visualisation()
-    # print(visualisation.controller.__dict__)
+    print("__main__")
 
     document.getElementById("startButton").addEventListener(
         "click", create_proxy(visualisation.start)
+    )
+
+    document.getElementById("stepBackwardButton").addEventListener(
+        "click", create_proxy(visualisation.backward_step)
+    )
+
+    document.getElementById("nextBackwardButton").addEventListener(
+        "click", create_proxy(visualisation.backward_next)
+    )
+
+    document.getElementById("stepForwardButton").addEventListener(
+        "click", create_proxy(visualisation.forward_step)
+    )
+
+    document.getElementById("nextForwardButton").addEventListener(
+        "click", create_proxy(visualisation.forward_next)
     )
